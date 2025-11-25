1d9ad67e18bb24dca36f1dcfeed0a55c72489e52
Maksim Shkryabin <maks4k32rus@gmail.com>
2025-11-25 13:34:27 +0300

    Сделал десктопную версию

---
+++ script.js
@@ -0,0 +1,15 @@
+let timeOut;
+
+const whatsAppUpdate = () => {
+  clearTimeout(timeOut);
+  timeOut = setTimeout(() => {
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const text = `Здравствуйте, меня зовут ${encodeURIComponent(name)}. Мой телефон ${encodeURIComponent(phone)}`;
+    const whatsappUrl = `https://wa.me/79017202459?text=${text}`;
+    document.getElementById("whatsappButton").setAttribute("href", whatsappUrl);
+  }, 300);
+};
+
+document.getElementById("name").addEventListener("input", whatsAppUpdate);
+document.getElementById("phone").addEventListener("input", whatsAppUpdate);
